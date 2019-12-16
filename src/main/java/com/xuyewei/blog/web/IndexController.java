b6b59b48247c7bc94f1b639a7282a4daf5e67726
Xu Yewei <captainxyw@gmail.com>
2019-12-16 16:19:36 +0800

    日志处理

--- src/main/java/com/xuyewei/blog/web/IndexController.java
+++ src/main/java/com/xuyewei/blog/web/IndexController.java
@@ -1,8 +1,8 @@
 package com.xuyewei.blog.web;
 
-import com.xuyewei.blog.NotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 /**
  * ClassName:IndexController
@@ -15,13 +15,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 
-    @GetMapping("/")
-    public String inddex() {
-//        int i = 9/0;
-        String blog = null;
-        if(blog == null) {
-            throw new NotFoundException("博客不存在");
-        }
+    @GetMapping("/{id}/{name}")
+    public String index(@PathVariable Integer id, @PathVariable String name) {
+////        int i = 9/0;
+//        String blog = null;
+//        if(blog == null) {
+//            throw new NotFoundException("博客不存在");
+//        }
+        System.out.println("--------------index-------------");
         return "index";
     }
 }

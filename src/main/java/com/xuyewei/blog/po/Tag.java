3d99c4db7a77767a749d7cb3724f919be26ac778
Xu Yewei <captainxyw@gmail.com>
2019-12-16 18:57:48 +0800

    实体类构建

---
+++ src/main/java/com/xuyewei/blog/po/Tag.java
@@ -0,0 +1,61 @@
+package com.xuyewei.blog.po;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * ClassName:Tag
+ * Package:com.xuyewei.blog.po
+ * Description:
+ *
+ * @Date:2019/12/16 17:53
+ * @Author:xuyewei
+ */
+
+@Entity
+@Table(name = "t_tag")
+public class Tag {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+
+    @ManyToMany(mappedBy = "tags")
+    private List<Blog> blogs = new ArrayList<>();
+
+    public Tag() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Blog> getBlogs() {
+        return blogs;
+    }
+
+    public void setBlogs(List<Blog> blogs) {
+        this.blogs = blogs;
+    }
+
+    @Override
+    public String toString() {
+        return "Tag{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}

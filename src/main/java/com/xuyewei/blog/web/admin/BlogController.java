165ebc11c5dfdea598ab234e9d5bb3c57d6823ea
Xu Yewei <captainxyw@gmail.com>
2019-12-19 20:09:03 +0800

    博客编辑

--- src/main/java/com/xuyewei/blog/web/admin/BlogController.java
+++ src/main/java/com/xuyewei/blog/web/admin/BlogController.java
@@ -13,6 +13,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -33,7 +34,7 @@ public class BlogController {
 
     private static final String INPUT = "admin/blogs-input";
     private static final String LIST = "admin/blogs";
-    private static final String REDIRECT_LIST = "redirect: /admin/blogs";
+    private static final String REDIRECT_LIST = "redirect:/admin/blogs";
     @Autowired
     private BlogService blogService;
     @Autowired
@@ -58,9 +59,25 @@ public class BlogController {
 
     @GetMapping("/blogs/input")
     public String input(Model model) {
+        setTypeAndTag(model);
+        Blog o = new Blog();
+
+        model.addAttribute("blog", o);
+        return INPUT;
+    }
+
+    private void setTypeAndTag(Model model) {
         model.addAttribute("types", typeService.listType());
         model.addAttribute("tags", tagService.listTag());
-        model.addAttribute("blog", new Blog());
+    }
+
+
+    @GetMapping("/blogs/{id}/input")
+    public String editInput(@PathVariable Long id, Model model) {
+        setTypeAndTag(model);
+        Blog blog = blogService.getBlog(id);
+        blog.init();
+        model.addAttribute("blog", blog);
         return INPUT;
     }
 
@@ -79,6 +96,14 @@ public class BlogController {
 
         }
 
+        return REDIRECT_LIST;
+    }
+
+
+    @GetMapping("/blogs/{id}/delete")
+    public String delete(@PathVariable Long id, RedirectAttributes attributes) {
+        blogService.deleteBlog(id);
+        attributes.addFlashAttribute("message", "删除成功");
         return REDIRECT_LIST;
     }
 
